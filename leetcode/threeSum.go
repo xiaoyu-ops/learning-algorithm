7701bf865587dbf95b05bcb1b60d12046b50b059
xiaoyu <wuzhy236@mail2.sysu.edu.cn>
2025-11-11 18:54:33 +0800

    三数之和完成

--- leetcode/threeSum.go
+++ leetcode/threeSum.go
@@ -6,16 +6,41 @@ import (
 
 func threeSum(nums []int) [][]int {
 	sort.Ints(nums)
-	check := map[int]int{}
-	
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			value = -(nums[i] + nums[j])
+	var result [][]int
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		left, right := i+1, len(nums)-1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum == 0 {
+				result = append(result, []int{nums[i], nums[left], nums[right]})
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				left++
+				right--
+			} else if sum < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+	return result
 }
-
+// 这个方法的精髓就是先排序，然后我们再锚定一个数假设其已经在结果集中
+// 然后用双指针法在剩下的数中寻找另外两个数，使得三数之和为0
+// 在寻找过程中，我们还要跳过重复的数以避免重复解
+// 注意最精髓的部分就是锚定这一步，因为锚定一个数后，问题就简化为在剩下的数
+// 中寻找两个数使得它们的和为一个固定值
 
 func main() {
 	nums := []int{-1, 0, 1, 2, -1, -4}
-	sort.Ints(nums)
-	fmt.Println(nums)
+	result := threeSum(nums)
+	fmt.Println(result)
 }
